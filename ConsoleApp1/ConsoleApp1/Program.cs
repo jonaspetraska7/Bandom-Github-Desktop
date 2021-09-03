7269580930bab38a961c049a517ed68d63063e32
jonaspetraska7 <60759096+jonaspetraska7@users.noreply.github.com>
2021-09-03 21:08:56 +0300

    Saugoju

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,18 +2,9 @@
 using System.Collections.Generic;
 
 namespace ConsoleApp1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            List<string> Sarasas = new List<string>();
 
-            for (int i = 0; i < Sarasas.Count; i++)
-            {
-                Console.WriteLine(Sarasas[i]);
-            }
-
-        }
-    }
+        ssdasd
+            fodar (asint i = 0; sd
+   d
 }
+asdasd
